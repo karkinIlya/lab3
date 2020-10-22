15362e7089f6e3dc3e192ee593d3e57f2165bb0a
Ilya Karkin <karkinia@yandex.ru>
2020-10-22 14:31:45 +0300

    Scripted auto-commit on change (2020-10-22 14:31:45) by gitwatch.sh

--- src/main/java/sparkJoinData/ReportApp.java
+++ src/main/java/sparkJoinData/ReportApp.java
@@ -4,6 +4,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 import java.util.Arrays;
 
@@ -19,7 +20,9 @@ public class ReportApp {
         JavaRDD<String> airportIdLines = airportIdFile.flatMap(
                 s -> Arrays.stream(s.split("\n")).iterator()
         );
-        JavaPairRDD<Integer, String> 
+        JavaPairRDD<Integer, String> airportCodeDescription = airportIdLines.mapToPair(
+                s -> new Tuple2<>(s.)
+        );
 
     }
 }
