4fd1f892b8d1d64a3dd72db01d690d528253818c
Ilya Karkin <karkinia@yandex.ru>
2020-10-22 14:19:20 +0300

    Scripted auto-commit on change (2020-10-22 14:19:20) by gitwatch.sh

--- src/main/java/sparkJoinData/ReportApp.java
+++ src/main/java/sparkJoinData/ReportApp.java
@@ -4,13 +4,19 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
+import java.util.Arrays;
+
 public class ReportApp {
     public static final String APPNAME = "Report airports";
+    public static final String AIRPORTIDFILE = "L_AIRPORT_ID.csv";
 
     public static void main(String[] args) throws Exception {
         SparkConf conf = new SparkConf().setAppName(APPNAME);
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<String> airportInfo = sc.textFile("L_AIRPORT_ID")
+        JavaRDD<String> airportIdFile = sc.textFile(AIRPORTIDFILE);
+        JavaRDD<String> splittedAirportId = airportIdFile.flatMap(
+                s -> Arrays.stream(s.split(""))
+        )
     }
 }
