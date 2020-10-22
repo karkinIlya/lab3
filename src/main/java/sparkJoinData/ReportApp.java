0f02f889347bdeeebedb1d6f66319f62be3d3fde
Ilya Karkin <karkinia@yandex.ru>
2020-10-22 14:15:21 +0300

    Scripted auto-commit on change (2020-10-22 14:15:21) by gitwatch.sh

--- src/main/java/sparkJoinData/ReportApp.java
+++ src/main/java/sparkJoinData/ReportApp.java
@@ -1,11 +1,15 @@
 package sparkJoinData;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
 
 public class ReportApp {
     public static final String APPNAME = "Report airports";
 
     public static void main(String[] args) throws Exception {
         SparkConf conf = new SparkConf().setAppName(APPNAME);
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> 
     }
 }
